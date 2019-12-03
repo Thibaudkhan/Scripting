44c2717496198453b633e7ca43fd4eb47b69f855
Antoine Lefebvre <antoine.lefebvre14@free.fr>
2019-12-03 23:06:20 +0100

    fix the stupidity of the last commit

--- Scripts/reader/python_application.py
+++ Scripts/reader/python_application.py
@@ -24,11 +24,11 @@ def detection_file():
     args = parser.parse_args().file.readlines()
 
     if ".json" in str(parser.parse_args()):
-        update_CONFIG(("".join(args).replace('"' or '\n' or ' ' or "}", '')).split(","))
+        update_CONFIG(("".join(args).replace('"', '').replace('\n', '').replace(' ', '').replace("}", "")).split(","))
     elif ".txt" in str(parser.parse_args()):
-        update_CONFIG(("-//-".join(args).replace('"' or '\n' or ' ' or "}", '')).split("-//-"))
+        update_CONFIG(("-//-".join(args).replace('"', '').replace('\n', '').replace(' ', '')).split("-//-"))
     elif ".yaml" in str(parser.parse_args()):
-        update_CONFIG(("-//-".join(args).replace('"' or '\n' or ' ' or "}", '')).split("-//-"))
+        update_CONFIG(("-//-".join(args).replace('"', '').replace('\n', '').replace(' ', '')).split("-//-"))
     else:
         print("ERROR type of file not found")
 
@@ -48,6 +48,8 @@ def main():
 
     for i, j in CONFIG.items():
         print(i, "=>", j)
+
+    print(" ")
 
 
 if __name__ == '__main__':
