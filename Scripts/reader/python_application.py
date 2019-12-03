4f46210f5dfb00160e21a269e134fbc3ca060812
Unknown <quentin.valbeny@edu.itescia.fr>
2019-12-03 14:53:28 +0100

    création dossier python_app

---
+++ Scripts/reader/python_application.py
@@ -0,0 +1,7 @@
+# Dans un repertoir
+# Detecter les fichers .txt .json et .yaml
+# L'application doit detecter quand il y a une modification dans les fichier
+# L'application doit copier les fichiers qui ont étaient modifier
+#Créer une log des modifications et l'afficher
+
+# Les signature (SHA256)
